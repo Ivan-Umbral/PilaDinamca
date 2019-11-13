1d1eadfd4a88fe4fc0196a12ecb4f865b30f1446
Ivan-Umbral <54869938+Ivan-Umbral@users.noreply.github.com>
2019-11-12 22:17:56 -0600

    Add files via upload

---
+++ PilaDinamica/src/piladinamica/PilaDinamica.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piladinamica;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ivan
+ */
+public class PilaDinamica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        int Opcion, a;
+        PilaVector objpila = new PilaVector();
+        do {
+            Opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "1. ingresar\n"+ "2. quitar\n"+ "3. cima\n"+ "4. vacia\n"+ "5. lmpiar\n"+ "6. exit\n"+"7. mostrar \n"));
+
+            switch (Opcion) {
+                case 1:
+                    a = Integer.parseInt(JOptionPane.showInputDialog(null, " ingrasa el numero a agragar"));
+                    objpila.insertar(a);
+                    break;
+                case 2:
+                    objpila.quitar();
+                    break;
+                case 3:
+                    objpila.cimaPila();
+                    break;
+                case 4:
+                    objpila.pilaVacia();
+                    break;
+                case 5:
+                    objpila.limpiarPila();
+                    break;
+                case 6:
+                    System.exit(0);
+                    break;
+                    case 7:
+                    objpila.mostrarPila();
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "INGRESE UNA OPCION VALIDA \n", "ERROR OPCION", JOptionPane.WARNING_MESSAGE);
+                    break;
+            }
+        } while (true);
+    }
+    }
+    
